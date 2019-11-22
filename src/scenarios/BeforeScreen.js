3ce6637a99219e13e8bacb5ba1637a70fb732c70
Sander Bougrine <sander.bougrine@student.howest.be>
2019-11-22 17:28:59 +0100

    WatchScreen

--- src/scenarios/BeforeScreen.js
+++ src/scenarios/BeforeScreen.js
@@ -4,7 +4,7 @@ import loading from '../img/loading.gif'
 
 export const BeforeScreen = () => {
     return (
-        <div>
+        <div style={{height:'100%'}}>
         	<h1 style={{padding:'9%'}}>Out of the Box</h1>
             <img src={image} style={{width:'100%', padding:'10%'}} />
             <p style={{fontWeight:'bold', padding:'10%', fontSize:'150%'}}>De voorstelling begint straks</p>
