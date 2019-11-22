8cd39c3017a751b3d845c91234054f74b98345fd
Fré Dumazy <dumazy@gmail.com>
2019-11-22 22:23:52 +0100

    Updat function

--- functions/index.js
+++ functions/index.js
@@ -13,7 +13,7 @@ const db = admin.firestore();
 
 exports.incrementAnswerCount = functions.firestore
     .document('performances/demo/scenarios/{scenarioId}/selected/{userId}')
-    .onWrite((change, context) => {
+    .onWrite(async (change, context) => {
         // Retrieve the current and previous value
       const data = change.after.data();
       const previousData = change.before.data();
@@ -24,14 +24,26 @@ exports.incrementAnswerCount = functions.firestore
 
       const questionId = context.params.scenarioId
       const answerId = data.answer;
+      console.log("questionId", questionId)
+      console.log("answerId", answerId);
     
-      const answer = db.doc(`performances/demo/scenarios/${questionId}/answers/${answerId}`)
-      const answerData = answer.data();
-      let count = answerData.count
-      if (!count) {
-        count = 0;
-      }
-      answer.set({
-          count: count + 1,
+      const answerRef = db.doc(`performances/demo/scenarios/${questionId}/answers/${answerId}`)
+      let count = await countAnswers(questionId, answerId)
+      answerRef.set({
+          count: count,
       }, {merge: true})
     });
+
+    const countAnswers = async (questionId, answerId) => {
+        const snap = await db
+            .collection(`performances/demo/scenarios/${questionId}/selected`)
+            .get()
+        let count = 0;
+        snap.forEach(doc => {
+            if(doc.data().answer == answerId) {
+                count++;
+            }
+        })
+        return count;
+
+    }
